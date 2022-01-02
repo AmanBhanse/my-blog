0e7f16114292b5f3b220bb2857e4d5466febc603
Aman Bhanse <amanbhanse22@gmail.com>
2022-01-02 11:52:32 +0530

    Add nodemon for auto update in server

--- src/server.js
+++ src/server.js
@@ -5,9 +5,19 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/hello", (req, res) => res.send("hello"));
-app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
+const articlesInfo = {
+    "learn-react": {
+        upvotes: 0,
+    },
+    "learn-node": {
+        upvotes: 0,
+    },
+};
 
-app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`));
+app.post("/api/articles/:name/upvote", (req, res) => {
+    const articleName = req.params.name;
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
+});
 
 app.listen(8000, () => console.log("Listening on port 8000"));
