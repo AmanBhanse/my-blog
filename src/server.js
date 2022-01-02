9e2ac511c3116003d64f85f223ed948bfeed803b
Aman Bhanse <amanbhanse22@gmail.com>
2022-01-02 17:31:41 +0530

    Add MongoDB to Express

--- src/server.js
+++ src/server.js
@@ -1,20 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { MongoClient } from "mongodb"; //Tool to connect to our local database
+
+const MONGO_DB_URL = "mongodb://127.0.0.1:27017";
+const MONGO_DB_NAME = "my-blog-db";
 
 const app = express();
 
 app.use(bodyParser.json());
 
-const articlesInfo = {
-    "learn-react": {
-        upvotes: 0,
-        comments: [],
-    },
-    "learn-node": {
-        upvotes: 0,
-        comments: [],
-    },
-};
+app.get("/api/articles/:name", async (req, res) => {
+    try {
+        const articleName = req.params.name;
+        const client = new MongoClient(MONGO_DB_URL);
+        await client.connect();
+        const db = client.db(MONGO_DB_NAME);
+        const articlesInfo = await db.collection("articles").findOne({ name: articleName });
+        res.status(200).json(articlesInfo);
+        client.close();
+    } catch (error) {
+        res.status(500).json({
+            message: "Error Connecting to DB",
+            error,
+        });
+    }
+});
 
 app.post("/api/articles/:name/upvote", (req, res) => {
     const articleName = req.params.name;
@@ -29,4 +39,5 @@ app.post("/api/articles/:name/add-comment", (req, res) => {
     articlesInfo[articleName].comments.push({ userName, text });
     res.status(200).send(articlesInfo[articleName]);
 });
+
 app.listen(8000, () => console.log("Listening on port 8000"));
